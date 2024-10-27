3064772a3f1ad71f35f063f88838a1aa6b3e157e
MahidharHub <g.m.mahidharreddy@gmail.com>
2024-10-27 21:33:28 +0100

    Modified to read csv file from inside JAR: 27-Oct-24

--- src/main/java/com/greenbone/task/samplecompany/util/ComputerCommandLineRunner.java
+++ src/main/java/com/greenbone/task/samplecompany/util/ComputerCommandLineRunner.java
@@ -7,13 +7,16 @@ import lombok.extern.java.Log;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
-import org.springframework.util.ResourceUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -26,13 +29,22 @@ public class ComputerCommandLineRunner implements CommandLineRunner {
     private ComputerService computerService;
 
     @Autowired
+    ResourceLoader resourceLoader;
+
+    @Autowired
     InformService informService;
 
     @Override
     public void run(String... args) throws Exception {
         log.info("Inside ComputerCommandLineRunner :: run");
-        File file = ResourceUtils.getFile("classpath:computers.csv");
+        Resource resource = resourceLoader.getResource("classpath:computers.csv");
+        InputStream inputStream = resource.getInputStream();
+        File file = new File("c1.csv");
+        FileUtils.copyInputStreamToFile(inputStream, file);
         Path path = file.toPath();
+
+
+
         try (
                 Reader reader = Files.newBufferedReader(path);
                 CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT.withFirstRecordAsHeader());
@@ -55,4 +67,5 @@ public class ComputerCommandLineRunner implements CommandLineRunner {
 
 
     }
+
 }
